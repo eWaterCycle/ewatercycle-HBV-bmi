81af70423289fd6bdfc228499151f009380fd892
David Haasnoot <d.b.haasnoot@student.tudelft.nl>
2024-02-09 11:41:26 +0100

    adding changes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Basic Model Interface (BMI) HBV model compatible with [eWaterCycle](https://github.com/eWaterCycle)
 
-HBV uses basic implementation with no snow component yet (v.0.0.1)
+HBV uses basic implementation with no snow component yet (v.0.1.4)
 
 Actual eWatercycle model wrapper can be found on [github](https://github.com/Daafip/ewatercycle-hbv)
 
--- src/HBV/HBV_bmi.py
+++ src/HBV/HBV_bmi.py
@@ -8,15 +8,25 @@ import json
 
 class HBV(Bmi):
     """HBV model wrapped in a BMI interface."""
-    
+
     def initialize(self, config_file: str) -> None:
+        """"Based on LeakyBucketBMI simple implementation of HBV without snow component
+            Requires atleast:
+            ---------------------
+            'precipitation_file': xarray with "pr" variable & time component
+            'potential_evaporation_file': xarray with "pev" variable of same nature as pr
+            'parameters': list of 8 parameters by a ','
+            'initial_storage' list of 4 storage parameters split by a ','
+
+        """
         # open json files containing data
         self.config: dict[str, Any] = utils.read_config(config_file)
 
         # store forcing & obs
         self.P = utils.load_var(self.config["precipitation_file"], "pr")
-        # self.EP = utils.load_var(self.config["potential_evaporation_file"], "pev")
-        self.EP = []
+
+        # add Tas, Tmin and Tmax support for snow component ??!
+        self.EP = utils.load_var(self.config["potential_evaporation_file"], "pev")
 
         # set up times
         self.Ts = self.P['time']
@@ -28,7 +38,7 @@ class HBV(Bmi):
 
         # define parameters 
         self.set_pars(np.array(self.config['parameters'].split(','),dtype=np.float64))
-        
+
         # add memory vector for tlag & run weights function
         self.memory_vector_lag = np.zeros(self.T_lag)
         self.weights = self.Weigfun()
@@ -40,12 +50,53 @@ class HBV(Bmi):
         # set other flows for initial step 
         self.Ei_dt     = 0       # interception evaporation
         self.Ea_dt     = 0       # actual evaportation
-        self.Qs_dt     = 0       # slow flow 
+        self.Qs_dt     = 0       # slow flow
         self.Qf_dt     = 0       # fast flow
         self.Q_tot_dt  = 0       # total flow
         self.Q_m       = 0       # Final model prediction
 
+        # stores the units for variables
+        self.dict_var_units = {
+                                "Imax":"mm",
+                                "Ce": "-",
+                                "Sumax": "mm",
+                                "Beta": "-",
+                                "Pmax": "mm",
+                                "Tlag": "d",
+                                "Kf": "-",
+                                "Ks": "-",
+                                "Si": "mm",
+                                "Su": "mm",
+                                "Sf": "mm",
+                                "Ss": "mm",
+                                "Ei_dt": "mm/d",
+                                "Ea_dt": "mm/d",
+                                "Qs_dt": "mm/d",
+                                "Qf_dt": "mm/d",
+                                "Q_tot_dt": "mm/d",
+                                "Q_m": "mm/d"}
+        # stores corresponding objects for variables
 
+        self.dict_var_obj = {
+                             "Imax": self.Imax,
+                             "Ce": self.Ce,
+                             "Sumax": self.Su_max,
+                             "Beta": self.beta,
+                             "Pmax": self.P_max,
+                             "Tlag":self.T_lag,
+                             "Kf": self.Kf,
+                             "Ks": self.ks,
+                             "Si": self.Si,
+                             "Su":self.Su,
+                             "Sf": self.Sf,
+                             "Ss":self.Ss,
+                             "Ei_dt": self.Ei_dt,
+                             "Ea_dt": self.Ei_dt,
+                             "Qs_dt": self.Qs_dt,
+                             "Qf_dt": self.Qf_dt,
+                             "Q_tot_dt": self.Q_tot_dt,
+                             "Q_m":self.Q_m,
+                             }
     def set_pars(self, par) -> None:
         # function to overwrite initial configuration of parameters, saves having to change the config file
         self.I_max  = par[0]                # maximum interception
@@ -61,12 +112,12 @@ class HBV(Bmi):
         self.Si = stor[0] # Interception storage
         self.Su = stor[1] # Unsaturated Rootzone Storage
         self.Sf = stor[2] # Fastflow storage
-        self.Ss = stor[3] # Groundwater storage       
-        
+        self.Ss = stor[3] # Groundwater storage
+
 
     def update(self) -> None:
         """
-        Function to run the updatre part of one timestep of the HBV model     
+        Function to run the update part of one timestep of the HBV model
         parameters
         ----------
         par: array_like
@@ -89,20 +140,20 @@ class HBV(Bmi):
         if self.current_timestep <= self.end_timestep:
             self.P_dt  = self.P.isel(time=self.current_timestep).to_numpy() * self.dt
             self.Ep_dt = self.EP.isel(time=self.current_timestep).to_numpy()  * self.dt
-        
+
             # Interception Reservoir
             if self.P_dt > 0:
                 # if there is rain, no evap
                 self.Si    = self.Si + self.P_dt               # increase the storage
                 self.Pe_dt = max((self.Si - self.I_max) / self.dt, 0)
-                self.Si    = self.Si - self.Pe_dt 
-                self.Ei_dt = 0                          # if rainfall, evaporation = 0 as too moist 
+                self.Si    = self.Si - self.Pe_dt
+                self.Ei_dt = 0                          # if rainfall, evaporation = 0 as too moist
             else:
                 # Evaporation only when there is no rainfall
                 self.Pe_dt    = 0                      # nothing flows in so must be 0
                 self.Ei_dt    = min(self.Ep_dt, self.Si / self.dt) # evaporation limited by storage
-                self.Si    = self.Si - self.Ei_dt 
-        
+                self.Si    = self.Si - self.Ei_dt
+
             # split flow into Unsaturated Reservoir and Fast flow
             if self.Pe_dt > 0:
                 cr       = (self.Su / self.Su_max)**self.beta
@@ -111,68 +162,68 @@ class HBV(Bmi):
                 Quf_dt   = cr  * self.Pe_dt            # flux from Su to Sf
             else:
                 Quf_dt = 0
-        
+
             # Transpiration
-            self.Ep_dt = max(0, self.Ep_dt - self.Ei_dt)        # Transpiration 
+            self.Ep_dt = max(0, self.Ep_dt - self.Ei_dt)        # Transpiration
             self.Ea_dt = self.Ep_dt  * (self.Su / (self.Su_max * self.Ce))
-            self.Ea_dt = min(self.Su, self.Ea_dt)            # limited by water in soil 
+            self.Ea_dt = min(self.Su, self.Ea_dt)            # limited by water in soil
             self.Su    = self.Su - self.Ea_dt
-        
+
             # Percolation
             Qus_dt = self.P_max * (self.Su / self.Su_max) * self.dt # Flux from Su to Ss
             self.Su  = self.Su - Qus_dt
-        
+
             # Fast Reservoir
             self.Sf = self.Sf + Quf_dt
             self.Qf_dt = self.dt * self.Kf * self.Sf
             self.Sf = self.Sf - self.Qf_dt
-        
+
             # Slow Reservoir
             self.Ss = self.Ss + Qus_dt
             self.Qs_dt = self.Ss * self.Ks * self.dt
             self.Ss = self.Ss - self.Qs_dt
-    
+
             # total = fast + slow
             self.Q_tot_dt = self.Qs_dt + self.Qf_dt
             # add time lag to the process
             self.add_time_lag()
             # self.Q_m = self.Q_tot_dt
-            
+
             # Advance the model time by one step
             self.current_timestep += 1
 
-    def Weigfun(self): 
+    def Weigfun(self):
         nmax=int(np.ceil(self.T_lag))
-        if nmax==1: 
+        if nmax==1:
             Weigths=float(1)
         else:
             Weigths=np.zeros(nmax)
             th=self.T_lag/2
             nh=int(np.floor(th))
-            for i in range(0,nh): 
-                Weigths[i]=(float(i+1)-0.5)/th         
+            for i in range(0,nh):
+                Weigths[i]=(float(i+1)-0.5)/th
             i=nh
-    
+
             Weigths[i]=(1+(float(i+1)-1)/th)*(th-int(np.floor(th)))/2+(1+(self.T_lag-float(i+1))/th)*(int(np.floor(th))+1-th)/2
             for i in range(nh+1, int(np.floor(self.T_lag))):
                 Weigths[i]=(self.T_lag-float(i+1)+.5)/th
-    
+
             if self.T_lag>int(np.floor(self.T_lag)):
                 Weigths[int(np.floor(self.T_lag))]=(self.T_lag-int(np.floor(self.T_lag)))**2/(2*th)
-    
+
             Weigths=Weigths/sum(Weigths)
-        
+
         return Weigths
-    
+
     def add_time_lag(self) -> None:
         # with support for T_lag =0
-        if len(self.memory_vector_lag) > 0:        
+        if len(self.memory_vector_lag) > 0:
             # Distribute current Q_tot_dt to memory vector
             self.memory_vector_lag += self.weights*self.Q_tot_dt
-            
+
             # Extract the latest Qm
             self.Q_m = self.memory_vector_lag[0]
-                     
+
             # Make a forecast to the next time step
             self.memory_vector_lag = np.roll(self.memory_vector_lag,-1)  # This cycles the array [1,2,3,4] becomes [2,3,4,1]
             self.memory_vector_lag[-1] = 0                              # the next last entry becomes 0 (outside of convolution lag)
@@ -182,61 +233,9 @@ class HBV(Bmi):
 
     def get_value(self, var_name: str, dest: np.ndarray) -> np.ndarray:
         # verander naar lookup
-        if(var_name == "Imax"):
-            dest[:] = np.array(self.Imax)
+        if var_name in self.dict_var_obj:
+            dest[:] = np.array(self.dict_var_obj[var_name])
             return dest
-        elif(var_name == "Ce"):
-            dest[:] = np.array(self.Ce)
-            return dest
-        elif(var_name == "Sumax"):
-            dest[:] = np.array(self.Su_max)
-            return dest
-        elif(var_name == "Beta"):
-            dest[:] = np.array(self.beta)
-            return dest
-        elif(var_name == "Pmax"):
-            dest[:] = np.array(self.P_max)
-            return dest
-        elif(var_name == "Tlag"):
-            dest[:] = np.array(int(self.T_lag))
-            return dest
-        elif(var_name == "Kf"):
-            dest[:] = np.array(self.Kf)
-            return dest    
-        elif(var_name == "Ks"):
-            dest[:] = np.array(self.ks)
-            return dest
-        elif(var_name == "Si"):
-            dest[:] = np.array(self.Si)
-            return dest
-        elif(var_name == "Su"):
-            dest[:] = np.array(self.Su)
-            return dest
-        elif(var_name == "Sf"):
-            dest[:] = np.array(self.Sf)
-            return dest
-        elif(var_name == "Ss"):
-            dest[:] = np.array(self.Ss)
-            return dest
-        elif(var_name == "Ei_dt"):
-            dest[:] = np.array(self.Ei_dt)
-            return dest
-        elif(var_name == "Ea_dt"):
-            dest[:] = np.array(self.Ei_dt)
-            return dest
-        elif(var_name == "Qs_dt"):
-            dest[:] = np.array(self.Qs_dt)
-            return dest
-        elif(var_name == "Qf_dt"):
-            dest[:] = np.array(self.Qf_dt)
-            return dest
-        elif(var_name == "Q_tot_dt"):
-            dest[:] = np.array(self.Q_tot_dt)
-            return dest
-        elif(var_name == "Q_m"):
-            dest[:] = np.array(self.Q_m)
-            return dest
-
         else:
             raise ValueError(f"Unknown variable {var_name}")
 
@@ -250,77 +249,26 @@ class HBV(Bmi):
 # elif (var_name == "parameters"):
 # dest[:] = np.array([self.I_max, self.Ce, self.Su_max, self.beta, self.P_max, self.T_lag, self.Kf, self.Ks])
 # return dest
-    dict_var_units = {
-        "Imax": "mm",
-        "Ce": "-",
-        "Sumax": "mm",
-        "Beta": "-",
-        "Pmax": "mm",
-        "Tlag": "d",
-        "Kf": "-",
-        "Ks": "-",
-        "Si": "mm",
-        "Su": "mm",
-        "Sf": "mm",
-        "Ss": "mm",
-        "Ei_dt": "mm/d",
-        "Ea_dt": "mm/d",
-        "Qs_dt": "mm/d",
-        "Qf_dt": "mm/d",
-        "Q_tot_dt": "mm/d",
-        "Q_m": "mm/d"}
-
     def get_var_units(self, var_name: str) -> str:
-        # loop up table
-        if var_name in dict_var_units.keys():
-            return dict_var_units[var_name]
+        # look up table
+        if var_name in self.dict_var_units:
+            return self.dict_var_units[var_name]
         else:
             raise ValueError(f"Unknown variable {var_name}")
 
     def set_value(self, var_name: str, src: np.ndarray) -> None:
-        if(var_name == "Imax"):
-            self.Imax = src[0]
-        elif(var_name == "Ce"):
-            self.Ce = src[0]
-        elif(var_name == "Sumax"):
-            self.Su_max = src[0]
-        elif(var_name == "Beta"):
-            self.beta = src[0]
-        elif(var_name == "Pmax"):
-            self.P_max = src[0]
-        elif(var_name == "Tlag"):
-            self.T_lag = self.set_tlag(src[0])
-        elif(var_name == "Kf"):
-            self.Kf = src[0]
-        elif(var_name == "Ks"):
-            self.Ks = src[0]
-        elif(var_name == "Si"):
-            self.Si = src[0]
-        elif(var_name == "Su"):
-            self.Su = src[0]
-        elif(var_name == "Sf"):
-            self.Sf = src[0]
-        elif(var_name == "Ss"):
-            self.Ss = src[0]
-        elif(var_name == "Ei_dt"):
-            self.Ei_dt = src[0]
-        elif(var_name == "Ea_dt"):
-            self.Ea_dt = src[0]
-        elif(var_name == "Qs_dt"):
-            self.Qs_dt = src[0]
-        elif(var_name == "Qf_dt"):
-            self.Qf_ft = src[0]
-        elif(var_name == "Q_tot_dt"):
-            self.Q_tot_dt = src[0]
-        elif(var_name == "Q_m"):
-            self.Qm = src[0]
-        elif(var_name == "memory_vector_lag"):
-            self.memory_vector_lag = src
+        if var_name == "Tlag":
+            self.set_tlag(src[0])
+        elif var_name in self.dict_var_obj:
+            self.dict_var_obj[var_name] = src[0]
+        ### add way to update memory vectory ?
+        # elif(var_name == "memory_vector_lag"):
+        #     self.memory_vector_lag = src
         else:
             raise ValueError(f"Unknown variable {var_name}")
 
     def get_output_var_names(self) -> Tuple[str]:
-        return dict_var_units.keys()
+        return self.dict_var_units.keys()
 
     # The BMI has to have some time-related functionality:
     def get_start_time(self) -> float:
@@ -335,13 +283,13 @@ class HBV(Bmi):
         """Return current time in seconds since 1 january 1970."""
         # we get the timestep from the data, but the stopping condition requires it to go one beyond. 
         return get_unixtime(self.Ts.isel(time=self.current_timestep).values) # type: ignore
-    
+
     def set_tlag(self, T_lag) -> int:
-        "Ensures T_lag is an integer"
+        """Ensures T_lag is an integer"""
         if type(T_lag) != int:
-            return int(T_lag)
+            self.T_lag = (T_lag)
         else:
-            return T_lag
+            self.T_lag = (T_lag)
 
     def get_time_step(self) -> float:
         if len(self.Ts) > 1:
