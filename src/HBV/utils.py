7c237efdc29f3875d05126890bd82373a13bb422
Daafip <daafips@gmail.com>
2024-05-13 15:18:54 +0200

    remove netcdf5 engine

--- src/HBV/utils.py
+++ src/HBV/utils.py
@@ -37,7 +37,7 @@ def load_var(ncfile: str | Path, varname: str) -> xr.DataArray:
         data = load_precip(forcing.directory / forcing.pr)
     """
     with dask.config.set(scheduler="synchronous"):
-        data = xr.open_dataset(ncfile, engine="h5netcdf")
+        data = xr.open_dataset(ncfile)
     assert "time" in data.dims
     assert varname in data.data_vars
     return data[varname]
